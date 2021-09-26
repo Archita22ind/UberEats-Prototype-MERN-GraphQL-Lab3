e8af6debc88f206d1a9b4b7e42fa6b3964facc38
Archita Chakraborty <archita22ind@gmail.com>
2021-09-25 17:26:44 -0700

    Typeahead completed

--- Frontend/src/common/mainHeader.js
+++ Frontend/src/common/mainHeader.js
@@ -16,46 +16,55 @@ import CartCheckoutModal from "./cartCheckoutModal.js";
 import { Link } from "react-router-dom";
 
 const MainHeader = (props) => {
+  let isTypeaheadUsed = false;
   let locationName = "San Jose";
   const [modalShow, setModalShow] = useState(false);
-  const [typeaheadInput, setTypeaheadInput] = useState("");
+  // const [typeaheadInput, setTypeaheadInput] = useState("");
   const [typeaheadOutput, setTypeaheadOutput] = useState([]);
+  const [valueSelected, setValueSelected] = useState({});
 
   const inputChangeHandler = (input, event) => {
     event.preventDefault();
 
-    setTypeaheadInput((prevState) => {
-      return input;
-    });
+    // setTypeaheadInput(input);
+    console.log("checking typeahead input", input);
+    optionDislayHandler(input);
+    // I cannot all this function here, BAD PRACTICE,
+  };
+  // console.log("typeahead input value", typeaheadInput);
+  const onChangeHandler = (selected) => {
+    // console.log("kya selected", selected);
+    setValueSelected(selected);
 
-    optionDislayHandler();
+    if (selected[0].isRestaurant) {
+      //redirect to restaurant details page
+    } else {
+      //send data to API call fetchFilteredRestaurants
+      // props.fetchFilteredRestaurants(true, selected[0].id)
+    }
   };
 
-  console.log("typeahead input value", typeaheadInput);
-
-  const onChangeHandler = (event) => {};
-
-  const optionDislayHandler = async () => {
+  const optionDislayHandler = async (inputssss) => {
     try {
       const response = await fetch("http://10.0.0.8:8080/getTypeaheadList", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
-          input: typeaheadInput,
+          input: inputssss,
         }),
       });
 
       let data = await response.json();
       console.log("my data", data);
 
-      // setTypeaheadOutput(() => {
-      //   return data;
-      // });
+      setTypeaheadOutput(() => {
+        return data;
+      });
     } catch (error) {
       console.log(error);
     }
   };
-  // console.log("Archita", typeaheadOutput);
+  console.log("Archita", typeaheadOutput);
 
   return (
     <Container fluid className="my-3">
@@ -95,17 +104,15 @@ const MainHeader = (props) => {
         <Col className="mt-3" xs={12} md={3}>
           <Typeahead
             id="id123"
+            labelKey={(option) => `${option.name}`}
+            // labelKey="label"
             maxResults={8}
             paginate={false}
             placeholder="What are you craving for?"
             onChange={onChangeHandler}
             onInputChange={inputChangeHandler}
-            // onChange={(selected) => {
-            //   // this.setState({selected});
-            // }}
-
-            options={[]}
-            selected={[]}
+            options={typeaheadOutput}
+            selected={valueSelected.name}
           />
         </Col>
         <Col className="mt-3" xs={4} md={2}>
